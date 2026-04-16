9e104a98962aa9030e7e718487775603c1cc7c22
RishabhWDB <rishabharya.k@gmail.com>
2026-04-16 12:17:16 +0530

    Added features

--- create_data.py
+++ create_data.py
@@ -10,6 +10,9 @@ n = 2000
 transaction_id = np.arange(1 , n + 1)
 amount = np.round(np.random.exponential(scale = 200 , size = n), 2)
 is_fraud = np.random.choice([0,1], size = n, p = [0.95,0.05])
+num_transactions_24h = np.random.poisson(lam=2, size=n)
+distance_from_home_km = np.round(np.random.exponential(scale=30, size=n), 2)
+is_weekend = np.random.choice([0, 1], size=n, p=[5/7, 2/7])
 
 neg_indices = np.random.choice(n, size = 10, replace = False)
 amount[neg_indices] = -np.abs(amount[neg_indices])
@@ -17,6 +20,9 @@ amount[neg_indices] = -np.abs(amount[neg_indices])
 df = pd.DataFrame({
     "transactions_id" : transaction_id,
     "amount" : amount,
+    "num_transactions_24h": num_transactions_24h,
+    "distance_from_home_km": distance_from_home_km,
+    "is_weekend": is_weekend,
     "is_fraud" : is_fraud
 })
 
